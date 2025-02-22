f9de0441f8b30b12c0b68be27f9dcf656145ba79
carth501 <carth501@gmail.com>
2025-02-22 15:10:35 -0500

    initialize project with React, TypeScript, and Vite setup

---
+++ super-nonogram/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center h-screen w-screen">
+        <h1>Super Nonogram</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
